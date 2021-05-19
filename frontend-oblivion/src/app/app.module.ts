05d468742d46f41219192937157ba1efb1a08bd3
Jossivon <41332492+Jossivon@users.noreply.github.com>
2021-05-19 14:05:41 -0500

    interfaces

--- frontend-oblivion/src/app/app.module.ts
+++ frontend-oblivion/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TecnicoComponent } from './tecnico/tecnico.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TecnicoComponent
   ],
   imports: [
     BrowserModule,
